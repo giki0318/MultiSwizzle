56ab229ccb48eb88395f1df23edbbe55f120cc0f
Marcus <wusuzhi@xunlei.com>
2017-12-06 21:07:04 +0800

    files

--- MultiSwizzle/ViewController+Swizzle1.m
+++ MultiSwizzle/ViewController+Swizzle1.m
@@ -7,7 +7,37 @@
 //
 
 #import "ViewController+Swizzle1.h"
+#import <objc/runtime.h>
 
 @implementation ViewController (Swizzle1)
+
++ (void)load {
+    [super load];
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        Class classname = [self class];
+        
+        SEL originSelector = @selector(viewDidLoad);
+        SEL swizzleSelector = @selector(qz_viewDidLoad);
+        
+        Method originMethod = class_getInstanceMethod(classname, originSelector);
+        Method swizzleMethod = class_getInstanceMethod(classname, swizzleSelector);
+        
+        BOOL didAddMethod = class_addMethod(classname, originSelector, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod));
+        if (didAddMethod) {
+            class_replaceMethod(classname, swizzleSelector, method_getImplementation(originMethod), method_getTypeEncoding(originMethod));
+        } else {
+            method_exchangeImplementations(originMethod, swizzleMethod);
+        }
+    });
+}
+
+- (void)qz_viewDidLoad {
+    NSLog(@"swizzle 1");
+    [self qz_viewDidLoad];
+}
+
 
 @end
--- MultiSwizzle/ViewController+Swizzle2.m
+++ MultiSwizzle/ViewController+Swizzle2.m
@@ -7,7 +7,36 @@
 //
 
 #import "ViewController+Swizzle2.h"
+#import <objc/runtime.h>
 
 @implementation ViewController (Swizzle2)
+
++ (void)load {
+    [super load];
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        Class classname = [self class];
+        
+        SEL originSelector = @selector(viewDidLoad);
+        SEL swizzleSelector = @selector(qz_viewDidLoad2);
+        
+        Method originMethod = class_getInstanceMethod(classname, originSelector);
+        Method swizzleMethod = class_getInstanceMethod(classname, swizzleSelector);
+        
+        BOOL didAddMethod = class_addMethod(classname, originSelector, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod));
+        if (didAddMethod) {
+            class_replaceMethod(classname, swizzleSelector, method_getImplementation(originMethod), method_getTypeEncoding(originMethod));
+        } else {
+            method_exchangeImplementations(originMethod, swizzleMethod);
+        }
+    });
+}
+
+- (void)qz_viewDidLoad2 {
+    NSLog(@"swizzle 2");
+    [self qz_viewDidLoad2];
+}
 
 @end
